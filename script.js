f8c28bf84e9abe3cfc2157ed50004fc0dff53208
Anton <anton.bayborosha@gmail.com>
2022-02-10 12:21:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const category = "toys";
+
+//console.log("https://someURL.com/".concat(category));
+
+document.write(`https://someURL.com/${category}/5`);
+document.writeln();
+
+const answr = [];
+answr[0] = "Carl";
+answr[1] = "Clar";
+answr[2] = " ";
+answr[3] = "ы";
+
+document.writeln(`${answr[0]}${answr[2]}${answr[1]}${answr[3]}`);
